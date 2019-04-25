6357301f414b4db5cbe2fde2cfd23e4db83b0371
Harold <harverbo@hotmail.com>
2019-04-25 14:48:28 -0400

    Corrección

---
+++ precache-manifest.f270c475ae66b00334c48662fc7fba79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18055718e8c376b5e74d8b051fd31457",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77cdba1a822462a0718c",
+    "url": "/static/css/main.0010486f.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "77cdba1a822462a0718c",
+    "url": "/static/js/main.1e569fd5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
